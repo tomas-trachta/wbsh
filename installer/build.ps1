f835e2c3a57466d6f0cdc007c76f33e819294496
Tomáš Trachta <tomas.trachta@finforce.cz>
2026-05-18 14:33:58 +0200

    v1.0.6

--- installer/build.ps1
+++ installer/build.ps1
@@ -11,7 +11,7 @@ param(
     [ValidateSet('Debug', 'Release')]
     [string]$Configuration = 'Release',
     [string]$Platform      = 'x64',
-    [string]$Version       = '1.0.5'
+    [string]$Version       = '1.0.6'
 )
 
 $ErrorActionPreference = 'Stop'
